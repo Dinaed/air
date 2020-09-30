46eef2c919a7da1c997acbdc32752fef483c2a59
Dinaed <65166300+Dinaed@users.noreply.github.com>
2020-09-30 18:21:02 +0300

    Updates

---
+++ precache-manifest.37143ce9eeab9ec0ecf2bdd9bf4c99f7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2a630f7e0992515ad90a7ff1084587",
+    "url": "/air/index.html"
+  },
+  {
+    "revision": "b97f11df306c9a8e5e57",
+    "url": "/air/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "893838fddd90b674f315",
+    "url": "/air/static/css/main.22b5db25.chunk.css"
+  },
+  {
+    "revision": "b97f11df306c9a8e5e57",
+    "url": "/air/static/js/2.d1c6b76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/air/static/js/2.d1c6b76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893838fddd90b674f315",
+    "url": "/air/static/js/main.0f306080.chunk.js"
+  },
+  {
+    "revision": "df68bbc94b5ce2226899",
+    "url": "/air/static/js/runtime-main.3c781981.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/air/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/air/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "4b0bd24be24ff5f21c5478e38de7224e",
+    "url": "/air/static/media/energetics.4b0bd24b.jpg"
+  },
+  {
+    "revision": "83d80abb4a07332d11ac8f71220f0020",
+    "url": "/air/static/media/energy.83d80abb.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/air/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "53f2c8030f7130e785004a1be9e92928",
+    "url": "/air/static/media/industry.53f2c803.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/air/static/media/shid_emozhl_logo.341a487c.svg"
+  },
+  {
+    "revision": "f2d8eacd69db6324f40c4a071cb6c335",
+    "url": "/air/static/media/sity_planing.f2d8eacd.jpg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/air/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/air/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/air/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/air/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/air/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "ef851caee62a0f820442bd58264222ef",
+    "url": "/air/static/media/transport.ef851cae.jpg"
+  },
+  {
+    "revision": "680938c5d002752f2f8898612b5d0f5c",
+    "url": "/air/static/media/utilisation.680938c5.jpg"
+  }
+]);
